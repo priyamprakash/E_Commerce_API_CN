0347522cb81b8672d338850fdccdc9cb67ca811d
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-16 02:32:48 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,7 @@ const productController = require('../controllers/product_controller');
 /** --------- MAKING ROUTES --------- **/
 router.get('/', productController.getProducts);
 router.post('/create', productController.create);
-router.post('/:id/quant', productController.update);
+router.post('/:id/update_quantity', productController.update);
 router.delete('/:id', productController.delete);
 
 
